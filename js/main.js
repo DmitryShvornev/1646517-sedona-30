be47b06d91ee45dd0bc573e814ef3b88c14c97fe
shvor <shvornev.dmitry@yandex.ru>
2021-03-08 13:51:09 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-const hotelSearch = document.querySelector('.hotel-search');
-const hotelForm = document.querySelector('.hotel-form');
-const hotelFormArrival = hotelForm.querySelector('[name=arrival]');
-const hotelFormDeparture = hotelForm.querySelector('[name=departure]');
-const hotelFormAdults = hotelForm.querySelector('[name=adults]');
-const hotelFormChildren = hotelForm.querySelector('[name=children]');
+const hotelSearch = document.querySelector(".hotel-search");
+const hotelForm = document.querySelector(".hotel-form");
+const hotelFormArrival = hotelForm.querySelector("[name=arrival]");
+const hotelFormDeparture = hotelForm.querySelector("[name=departure]");
+const hotelFormAdults = hotelForm.querySelector("[name=adults]");
+const hotelFormChildren = hotelForm.querySelector("[name=children]");
 
 let isStorageSupport = true;
 let adults = '';
 let children = '';
 
 try {
-  adults = localStorage.getItem('adults');
-  children = localStorage.getItem('children');
+  adults = localStorage.getItem("adults");
+  children = localStorage.getItem("children");
 } catch (err) {
   isStorageSupport = false;
 }
 
-hotelForm.classList.add('hotel-form-hide');
+hotelForm.classList.add("hotel-form-hide");
 
-hotelSearch.addEventListener('click', function (evt) {
+hotelSearch.addEventListener("click", function (evt) {
   evt.preventDefault();
-  hotelForm.classList.remove('hotel-form-error');
-  hotelForm.classList.toggle('hotel-form-show');
+  hotelForm.classList.remove("hotel-form-error");
+  hotelForm.classList.toggle("hotel-form-show");
   if (adults && children) {
     hotelFormAdults.value = adults;
     hotelFormChildren.value = children;
   }
 });
 
-hotelForm.addEventListener('submit', function (evt) {
+hotelForm.addEventListener("submit", function (evt) {
   if (!hotelFormArrival.value || !hotelFormDeparture.value || !hotelFormAdults.value || !hotelFormChildren.value) {
     evt.preventDefault();
-    hotelForm.classList.remove('hotel-form-error');
+    hotelForm.classList.remove("hotel-form-error");
     hotelForm.offsetWidth = hotelForm.offsetWidth;
-    hotelForm.classList.add('hotel-form-error');
+    hotelForm.classList.add("hotel-form-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem('adults', hotelFormAdults.value);
-      localStorage.setItem('children', hotelFormChildren.value);
+      localStorage.setItem("adults", hotelFormAdults.value);
+      localStorage.setItem("children", hotelFormChildren.value);
     }
   }
 });
 
 
-if (hotelForm.classList.contains('hotel-form-error')) {
-  hotelForm.classList.remove('hotel-form-error');
+if (hotelForm.classList.contains("hotel-form-error")) {
+  hotelForm.classList.remove("hotel-form-error");
 }
